a4c5e29f35dc614c0923b06fa47be1c9a0602fda
Kyle Cesare <kcesare@gmail.com>
2013-04-14 20:18:04 -0700

    Add basic route handling.

--- lib/w.rb
+++ lib/w.rb
@@ -4,5 +4,22 @@ require 'bundler/setup'
 require 'rack'
 
 module W
+  @@routes = {}
 
+  [:get, :post].each do |m|
+    @@routes[m] = []
+  end
+
+  # Define a route for GET requests for the given +path+.  You can pass in a
+  # regexp to match on parameters.  Pass in a block as a callback for the route.
+  # Parameters will be passed back to the block:
+  #
+  #   get "/foo/(.+)" do |id|
+  #     "Got a request for #{id}!"
+  #   end
+  #
+  # The value returned by the block will be rendered.
+  def get(path, &blk)
+    @@routes[:get] << { :path => path, :behavior => blk }
+  end
 end
